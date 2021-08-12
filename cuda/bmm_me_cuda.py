bb4b55a921f8a59b5edb0ff0b262d823cb1a70d1
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-08-12 19:39:48 -0400

    d

---
+++ cuda/bmm_me_cuda.py
@@ -0,0 +1,15 @@
+import math
+import torch
+
+import bmm_me_cuda
+
+torch.manual_seed(42)
+
+
+
+class bmm_me():
+    def __init__(self):
+
+
+    def forward(self, A, B):
+        return bmm_me.forward(A, B)

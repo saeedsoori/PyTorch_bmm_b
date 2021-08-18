6714c201f9a81b1224dee3f4caf8a984de29f99d
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-08-17 20:21:15 -0400

    new

--- cuda/setup.py
+++ cuda/setup.py
@@ -7,7 +7,7 @@ setup(
         CUDAExtension('bmm_cuda', [
             'bmm_cuda.cpp',
             'bmm_cuda_kernel.cu',],
-            extra_compile_args={'nvcc':['-O3','-L/content/magma-2.5.4/lib -lmagma_sparse -lmagma','-L/usr/local/cuda/lib64  -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-I/usr/local/cuda/include', '-I/content/magma-2.5.4/include -I/content/magma-2.5.4/control -I/content/magma-2.5.4/magmablas', '-DADD_']},
+            extra_compile_args={'nvcc':['-O3','-L/content/magma-2.5.4/lib -lmagma_sparse -lmagma','-L/usr/local/cuda/lib64  -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-I/usr/local/cuda/include', '-I/content/magma-2.5.4/include' ,'-I/content/magma-2.5.4/control', '-I/content/magma-2.5.4/magmablas', '-DADD_']},
             ),
     ],
     cmdclass={

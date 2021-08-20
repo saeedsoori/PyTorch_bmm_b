bfb416bec7e669403d43fe0f9b69cbe8160ff73b
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-08-20 13:48:03 -0400

    fixing setup.py

--- cuda/setup.py
+++ cuda/setup.py
@@ -7,7 +7,7 @@ setup(
         CUDAExtension('bmm_cuda', [
             'bmm_cuda.cpp',
             'bmm_cuda_kernel.cpp',],
-            extra_compile_args={'gcc':['-O3','-L/home/ubuntu/magma/magma-2.5.4/lib/ -lmagma_sparse -lmagma', '-I/home/ubuntu/magma/magma-2.5.4/include' , '-I/usr/local/cuda/include', '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-DADD_'],} 
+            extra_compile_args={'cxx':['-O3','-L/home/ubuntu/magma/magma-2.5.4/lib/ -lmagma_sparse -lmagma', '-I/home/ubuntu/magma/magma-2.5.4/include' , '-I/usr/local/cuda/include', '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-DADD_'],} 
             ),
     ],
     cmdclass={

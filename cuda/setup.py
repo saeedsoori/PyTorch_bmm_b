053b7548ae9ec50e94bd9ef62ebb2b77f335bbf3
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-08-20 13:22:07 -0400

    adding magma_sparse lib

--- cuda/setup.py
+++ cuda/setup.py
@@ -7,7 +7,7 @@ setup(
         CUDAExtension('bmm_cuda', [
             'bmm_cuda.cpp',
             'bmm_cuda_kernel.cu',],
-            extra_compile_args={'nvcc':['-O3', '-DADD_ -Xcompiler "-fPIC -Wall -Wno-unused-function" -std=c++11  -gencode arch=compute_75,code=sm_75 -gencode arch=compute_75,code=compute_75', '-L/home/ubuntu/magma/magma-2.5.4/lib/  -lmagma', '-I/home/ubuntu/magma/magma-2.5.4/include/' , '-I/usr/local/cuda/include', '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-DADD_'], 
+            extra_compile_args={'nvcc':['-O3', '-DADD_ -Xcompiler "-fPIC -Wall -Wno-unused-function" -std=c++11  -gencode arch=compute_75,code=sm_75 -gencode arch=compute_75,code=compute_75', '-L/home/ubuntu/magma/magma-2.5.4/lib/  -lmagma_sparse -lmagma', '-I/home/ubuntu/magma/magma-2.5.4/include/' , '-I/usr/local/cuda/include', '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-DADD_'], 
             'gcc':['-O3','-L/home/ubuntu/magma/magma-2.5.4/lib/ -lmagma_sparse -lmagma', '-I/home/ubuntu/magma/magma-2.5.4/include' , '-I/usr/local/cuda/include', '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-DADD_'],} 
             ),
     ],

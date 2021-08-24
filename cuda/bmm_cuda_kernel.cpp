b4efab258e5fa8f6f4bd45eb428a5daef56462e2
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-08-24 17:30:41 -0400

    fixing the magma call

--- cuda/bmm_cuda_kernel.cpp
+++ cuda/bmm_cuda_kernel.cpp
@@ -165,54 +165,13 @@ float *h_A;
     h_C[i] = rand() / static_cast<float>(RAND_MAX);
   }
 
-  /* Allocate device memory for the matrices */
-  // if (cudaMalloc(reinterpret_cast<void **>(&d_A), n2 * sizeof(d_A[0])) !=
-  //     cudaSuccess) {
-  //   fprintf(stderr, "!!!! device memory allocation error (allocate A)\n");
-  //   return EXIT_FAILURE;
-  // }
-
-  // if (cudaMalloc(reinterpret_cast<void **>(&d_B), n2 * sizeof(d_B[0])) !=
-  //     cudaSuccess) {
-  //   fprintf(stderr, "!!!! device memory allocation error (allocate B)\n");
-  //   return EXIT_FAILURE;
-  // }
-
-  // if (cudaMalloc(reinterpret_cast<void **>(&d_C), n2 * sizeof(d_C[0])) !=
-  //     cudaSuccess) {
-  //   fprintf(stderr, "!!!! device memory allocation error (allocate C)\n");
-  //   return EXIT_FAILURE;
-  // }
-
-  /* Initialize the device matrices with the host matrices */
-  // status = cublasSetVector(n2, sizeof(h_A[0]), h_A, 1, d_A, 1);
-
-  // if (status != CUBLAS_STATUS_SUCCESS) {
-  //   fprintf(stderr, "!!!! device access error (write A)\n");
-  //   return EXIT_FAILURE;
-  // }
-
-  // status = cublasSetVector(n2, sizeof(h_B[0]), h_B, 1, d_B, 1);
-
-  // if (status != CUBLAS_STATUS_SUCCESS) {
-  //   fprintf(stderr, "!!!! device access error (write B)\n");
-  //   return EXIT_FAILURE;
-  // }
-
-  // status = cublasSetVector(n2, sizeof(h_C[0]), h_C, 1, d_C, 1);
-
-  // if (status != CUBLAS_STATUS_SUCCESS) {
-  //   fprintf(stderr, "!!!! device access error (write C)\n");
-  //   return EXIT_FAILURE;
-  // }
-
-  // /* Performs operation using plain C code */
-  // simple_sgemm(N, alpha, h_A, h_B, beta, h_C);
-  // h_C_ref = h_C;
-
+  
   
 
+status = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n[0], m[0], k[0], &alpha, dB_array[0],
+                       n[0], dA_array[0], k[0], &beta, dC_array[0], n[0]);
 
+std::cout<<"hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\n";
 
 
   /* Performs operation using cublas */

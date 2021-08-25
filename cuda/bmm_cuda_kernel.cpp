b6a10812d583a713b24b702308d565de85d9c5ff
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-08-24 23:41:30 -0400

    fixing the magma call

--- cuda/bmm_cuda_kernel.cpp
+++ cuda/bmm_cuda_kernel.cpp
@@ -135,7 +135,7 @@ float *h_A;
     fprintf(stderr, "!!!! CUBLAS initialization error\n");
     return EXIT_FAILURE;
   }
-
+  std::cout<<"\n";
   /* Allocate host memory for the matrices */
   h_A = reinterpret_cast<float *>(malloc(32 * sizeof(float*));
 

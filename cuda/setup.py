3c72e4e7fe844417b32741b318cca17e824a40e9
saeed soori <saeedsoori@saeeds-MacBook-Pro.local>
2021-08-26 21:03:59 -0400

    fixing setup.py

--- cuda/setup.py
+++ cuda/setup.py
@@ -6,7 +6,11 @@ setup(
     ext_modules=[
         CUDAExtension('bmm_cuda', [
             'bmm_cuda.cpp',],
-            extra_compile_args={'cxx':['-O3','-L/home/ubuntu/magma/magma-2.5.4/lib/ -lmagma_sparse -lmagma', '-I/home/ubuntu/magma/magma-2.5.4/include' , '-I/usr/local/cuda/include', '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-DADD_'],} 
+            extra_compile_args={'cxx':['-O3','-fopenmp', '-L/home/ubuntu/magma/magma-2.5.4/lib/ -lmagma_sparse -lmagma', '-I/home/ubuntu/magma/magma-2.5.4/include' , '-I/usr/local/cuda/include', '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', '-DADD_'],},
+             library_dirs=['/home/ubuntu/magma/magma-2.5.4/lib'],
+             libraries=['magma_sparse', 'magma'],
+             extra_ldflags={'cxx': [
+                          '-L/usr/local/cuda/lib64 -lcudart -lcudadevrt -lcublas -lcusparse -llapack -lblas -lpthread -lm', ], },
             ),
     ],
     cmdclass={
